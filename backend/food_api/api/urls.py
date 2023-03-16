b3c83dd14ad7a855f754c36d8fa02b634bebb2f5
DamirKasimov <d:\mail>
2023-03-16 14:24:56 +0400

    workflow deploying

--- backend/food_api/api/urls.py
+++ backend/food_api/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
-from users.views import CustomUserViewSet
 
-from .views import IngridientsViewSet, RecipeViewSet, TagsViewSet
+from .views import RecipeViewSet, TagsViewSet, IngridientsViewSet
+from users.views import CustomUserViewSet
 
 app_name = 'api'
 
@@ -12,13 +12,13 @@ router.register('tags', TagsViewSet, basename='tags')
 router.register('ingredients', IngridientsViewSet, basename='ingridients')
 router.register('recipes', RecipeViewSet, basename='recipes')
 
+
 urlpatterns = [
     path('', include(router.urls)),
     # + /?recipes_limit=...
     path('users/subscriptions/', CustomUserViewSet.as_view(
         {'get': 'subscriptions'}), name='subscriptions'),
     re_path(r'(/users/)?', include('djoser.urls')),
-
     # + /?recipes_limit=...
     path('users/<int:id>/subscribe/', CustomUserViewSet.as_view(
         {'post': 'subscribe', 'delete': 'subscribe'}), name='to_un/subscribe'),
